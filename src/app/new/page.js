46e26de69faf905a89022927410cad5bc3fd0a5a
romansalom <roman.salom12@gmail.com>
2023-12-01 15:35:44 -0300

    modifico

--- src/app/new/page.js
+++ src/app/new/page.js
@@ -49,23 +49,23 @@ export default function Page({params}) {
       }
     },[])
     return (
-      <div class="font-sans mt-20">
-      <div class="relative min-h-screen flex flex-col sm:justify-center items-center bg-gray-100">
-        <div class="relative sm:max-w-sm w-full">
-          <div class="card bg-blue-400 shadow-lg w-full h-full rounded-3xl absolute transform -rotate-6"></div>
-          <div class="card bg-red-400 shadow-lg w-full h-full rounded-3xl absolute transform rotate-6"></div>
-          <div class="relative w-full rounded-3xl  px-6 py-4 bg-gray-100 shadow-md">
-            <label for="" class="block mt-3 text-sm text-gray-700 text-center font-semibold">
+      <div className="font-sans mt-20">
+      <div className="relative min-h-screen flex flex-col sm:justify-center items-center bg-gray-100">
+        <div className="relative sm:max-w-sm w-full">
+          <div className="card bg-blue-400 shadow-lg w-full h-full rounded-3xl absolute transform -rotate-6"></div>
+          <div className="card bg-red-400 shadow-lg w-full h-full rounded-3xl absolute transform rotate-6"></div>
+          <div className="relative w-full rounded-3xl  px-6 py-4 bg-gray-100 shadow-md">
+            <label  className="block mt-3 text-sm text-gray-700 text-center font-semibold">
               {params.id ? 'Editar Tarea' : 'Crea Nueva Tarea'}
             </label>
     
-            <form onSubmit={onSubmit} class="mt-10">
+            <form onSubmit={onSubmit} className="mt-10">
     
               <div>
                 <input
                   placeholder="Escribe el titulo de la nueva tarea"
                   {...register('title', { required: true })}
-                  class="mt-1 block w-full border-none bg-gray-100 h-11 rounded-xl shadow-lg hover:bg-blue-100 focus:bg-blue-100 focus:ring-0"
+                  className="mt-1 block w-full border-none bg-gray-100 h-11 rounded-xl shadow-lg hover:bg-blue-100 focus:bg-blue-100 focus:ring-0"
                 />
                 {errors.title && (
                   <span>
@@ -74,11 +74,11 @@ export default function Page({params}) {
                 )}
               </div>
     
-              <div class="mt-7">
+              <div className="mt-7">
                 <textarea
                   placeholder="Escribe una descripcion de la nueva tarea"
                   {...register('description', { required: true })}
-                  class="mt-1 block w-full border-none bg-gray-100 h-11 rounded-xl shadow-lg hover:bg-blue-100 focus:bg-blue-100 focus:ring-0"
+                  className="mt-1 block w-full border-none bg-gray-100 h-11 rounded-xl shadow-lg hover:bg-blue-100 focus:bg-blue-100 focus:ring-0"
                 />
                 {errors.description && (
                   <span>
@@ -87,10 +87,10 @@ export default function Page({params}) {
                 )}
               </div>
     
-              <div class="mt-7">
+              <div className="mt-7">
                 <button
                   onClick={() => router.push('/new')}
-                  class="bg-blue-500 w-full py-3 rounded-xl text-white shadow-xl hover:shadow-inner focus:outline-none transition duration-500 ease-in-out transform hover:-translate-x hover:scale-105"
+                  className="bg-blue-500 w-full py-3 rounded-xl text-white shadow-xl hover:shadow-inner focus:outline-none transition duration-500 ease-in-out transform hover:-translate-x hover:scale-105"
                 >
                   {params.id ? 'Editar Tarea' : 'Crear Tarea'}
                 </button>
